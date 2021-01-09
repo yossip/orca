019c78a9b6003f87e665cc9d230ea2b0a976fc19
Yossi Makover Pepo <yossip@qwilt.com>
2021-01-09 06:50:10 +0200

    Add readme

--- Dockerfile
+++ Dockerfile
@@ -8,6 +8,8 @@ COPY . .
 
 RUN ["pip3", "install", "pipenv"]
 
+RUN apk update && apk add postgresql-dev gcc python3-dev musl-dev && pip install psycopg2
+
 RUN ["pipenv", "install"]
 
 CMD ./run_app.sh
--- config.py
+++ config.py
@@ -2,5 +2,10 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', f'postgres://{os.path.join(basedir, "app.db")}') or None
+    SQLALCHEMY_DATABASE_URI = ("postgres://"+ os.environ['DB_USER'] + ":" 
+                                         + os.environ['DB_PASS']+ "@" 
+                                         + os.environ['DB_HOST'] 
+                                         + ":5432/app.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
